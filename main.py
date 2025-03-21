248b447bea7ce4115c513928510c3aafb4937493
hoseinx <hoseinx@gmail.com>
2025-03-21 16:24:01 +0330

    edit

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from tasks import router as tasks_router
 from products import router as products_router
 from auth import router as auth_router  # Import auth routes
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+# List of origins that are allowed to make requests to the API
+origins = [
+    "http://localhost:3000",  # React app running on localhost:3000
+    "http://127.0.0.1:3000",  # React app running on 127.0.0.1:3000 (if needed)
+]
+
+# Add CORSMiddleware to the app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows these origins to access the API
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Include routers from tasks and products
 app.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
